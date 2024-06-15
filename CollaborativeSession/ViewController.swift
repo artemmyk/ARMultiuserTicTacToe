bc2ab0aea0bc444bfa5eb0b1226571cf013470f6
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-06-15 16:13:56 -0400

    Enable people occlusion

--- CollaborativeSession/ViewController.swift
+++ CollaborativeSession/ViewController.swift
@@ -49,6 +49,9 @@ class ViewController: UIViewController, ARSessionDelegate {
         
         // Enable realistic reflections.
         configuration?.environmentTexturing = .automatic
+        
+        // Enable people occlusion
+        configuration?.frameSemantics.insert(.personSegmentationWithDepth)
 
         // Begin the session.
         arView.session.run(configuration!)
