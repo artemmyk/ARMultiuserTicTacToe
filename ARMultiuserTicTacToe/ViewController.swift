8fc02e1a29b360e98156119ef8157a16181e514a
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-06-24 16:31:42 -0400

    Add launch screen image. Final cleanup

--- ARMultiuserTicTacToe/ViewController.swift
+++ ARMultiuserTicTacToe/ViewController.swift
@@ -162,14 +162,6 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func goBackPressed() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        if let menuVC = storyboard.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController {
-            menuVC.modalPresentationStyle = .fullScreen
-            present(menuVC, animated: true, completion: nil)
-        }
-    }
-    
 }
 
 // MARK: - Game Mode Change
